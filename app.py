aac9f3355b6339527759e2cbe0b3a6836a6b30a1
rajjakmiah63-lgtm <rajjakmiah63@gmail.com>
2026-07-08 23:50:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,1 @@
-("My-Jarvis AI Starting...")print("My-Jarvis AI Starting...")
-
-name = input("Your name: ")
-print("Hello,", name)
-question = input("Ask me anything: ")
-
-if "hello" in question.lower():
-    print("Hello! How can I help you?")
-elif "your name" in question.lower():
-    print("I am My-Jarvis AI.")
-else:
-    print("Sorry, I'm still learning.")
+print("My-Jarvis AI Starting...")
